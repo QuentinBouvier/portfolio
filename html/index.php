eb8a6222316af4322f8f0a583cd0d65bfbae09ab
BabbleProd <32564163+QuentinBouvier@users.noreply.github.com>
2017-10-27 15:26:57 +0200

    changed file structure and added a merging index.php

---
+++ html/index.php
@@ -0,0 +1,13 @@
+<?php
+
+//template
+require_once("./templates/head.html");
+require_once("./templates/nav.html");
+
+// content
+require_once("./pages/banner.html");
+require_once("./pages/works.html");
+require_once("./pages/CV.html");
+
+// template
+require_once("./templates/foot.html");
