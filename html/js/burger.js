773ac83cd00d8d45ec0bb75347c5020d4f2cbba7
QuentinBouvier <32564163+QuentinBouvier@users.noreply.github.com>
2017-11-02 00:04:54 +0100

    improved expand menu behavior, added on scroll event for burgerized

--- html/js/burger.js
+++ html/js/burger.js
@@ -1,5 +1,8 @@
+// Listeners
 $(document).ready(function()
 {
+    burgerize();
+
     // attempt a burgerize on window resize
     $(window).resize(function()
     {
@@ -15,45 +18,70 @@ $(document).ready(function()
         }
     });
 
+    $('body').on('changedActive', '[data-menu]', function()
+    {
+        burgerizeScrollBehavior($(this));
+    });
+
     // expand menu on navbar button click
-    $('div[data-navbar="button"]').click(function()
+    $('body').on('click', '[data-navbar="button"]', function()
     {
         expandMenu();
     });
+});
 
-    function expandMenu()
+function expandMenu()
+{
+    $('[data-navbar]').each(function()
     {
-        $('[data-navbar]').each(function()
-        {
-            $(this).toggleClass('menu-expanded');
-        });
+        $(this).toggleClass('menu-expanded');
+    });
+}
+
+function burgerize()
+{
+    var master = $('div[data-navbar="master"]');
+    var container = $('ul[data-navbar="container"]');
+    var button = $('div[data-navbar="button"]');
+    var icon = $('div[data-navbar="icon"]');
+    var active = $('.navbar-link-active');
+
+    if (window.matchMedia("(min-width: 800px)").matches)
+    {
+        // is wider
+        container.removeClass('menu-burgerized');
+        container.addClass('ml-2');
+        master.removeClass('flex-justify-between');
+        master.removeClass('menu-expanded');
+        master.addClass('flex-justify-left');
+        button.addClass('hidden');
+        icon.removeClass('ml-1');
     }
-
-    function burgerize()
+    else
     {
-        var master = $('div[data-navbar="master"]');
-        var container = $('ul[data-navbar="container"]');
-        var button = $('div[data-navbar="button"]');
-        var active = $('.navbar-link-active');
+        // is smaller
+        container.addClass('menu-burgerized');
+        container.removeClass('ml-2');
+        master.addClass('flex-justify-between');
+        master.removeClass('flex-justify-left');
+        button.removeClass('hidden');
+        icon.addClass('ml-1');
+    }
+}
 
-        if (window.matchMedia("(min-width: 800px)").matches)
+function burgerizeScrollBehavior(activeLink)
+{
+    var output = activeLink.prop("outerHTML");
+    $.each($('[data-navbar="container"]').children(), function()
+    {
+        if ($(this).hasClass('navbar-link-active'))
         {
-            // is wider
-            container.removeClass('menu-burgerized');
-            master.removeClass('flex-justify-between');
-            master.removeClass('menu-expanded');
-            master.addClass('flex-justify-left');
-            button.addClass('hidden');
-            active.removeClass('mb-2');
+            return true;
         }
         else
         {
-            // is smaller
-            container.addClass('menu-burgerized');
-            master.addClass('flex-justify-between');
-            master.removeClass('flex-justify-left');
-            button.removeClass('hidden');
-            active.addClass('mb-2');
+            output += $(this).prop("outerHTML");
         }
-    }
-});
+    });
+    activeLink.parent().html(output);
+}
