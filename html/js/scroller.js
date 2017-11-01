862bb256d9e9433bbab6b6791583b6318c124d76
QuentinBouvier <32564163+QuentinBouvier@users.noreply.github.com>
2017-11-02 00:03:42 +0100

    modify on load behavior, changed selectors to data attr

--- html/js/scroller.js
+++ html/js/scroller.js
@@ -1,13 +1,17 @@
+// Listeners
 $(document).ready(function()
 {
-    var sectionsID = ['#home', '#works', '#curiculum', '#contact'];
-    var buttonClass = '.topbar-link';
-    var isScrolling = false;
+    selectButton($('[data-menu="0"]'));
 
     //click on navbar links
-    $('.topbar-link').on('click', function()
+    $('body').on('click', '[data-menu]', function()
     {
         scrollToElement($(this), sectionsID);
+    });
+
+    $('body').on('click', '[data-navbar="icon"]', function()
+    {
+        scrollToElement($('[data-menu="0"]'), sectionsID);
     });
 
     //on scroll event
@@ -15,87 +19,94 @@ $(document).ready(function()
     {
         selectButtonOnScroll(sectionsID);
     });
+});
 
-    /**
-     * Unselect a navigation bar button
-     * @param string buttonsClass class name of the buttons to handle starting with a dot (.)
-     * @param string buttonActiveClass class name on an active button
-     * @param string ignoreClass if you want buttons to be ignored on the bar, set a class name to ignore
-     */
-    function unselectButtons(buttonsClass, buttonActiveClass, ignoreClass)
+var sectionsID = ['#home', '#works', '#curiculum', '#contact'];
+var exclusionClass = '.social-icon';
+var isScrolling = false;
+
+
+/**
+ * Unselect a navigation bar button
+ * @param string buttonsClass class name of the buttons to handle starting with a dot (.)
+ * @param string buttonActiveClass class name on an active button
+ * @param string ignoreClass if you want buttons to be ignored on the bar, set a class name to ignore
+ */
+function unselectButtons(buttonsClass, buttonActiveClass, ignoreClass)
+{
+    //remove the highlighting class from every items of the navbar
+    $.each($(buttonsClass), function()
     {
-        //remove the highlighting class from every items of the navbar
-        $.each($(buttonsClass), function()
+        if ($(this).hasClass(buttonActiveClass) && !$(this).hasClass(ignoreClass))
         {
-            if ($(this).hasClass(buttonActiveClass) && !$(this).hasClass(ignoreClass))
+            $(this).removeClass(buttonActiveClass);
+        }
+    });
+}
+
+/**
+ * put class navbar-link-active on selected DOM element
+ * @param  {DOM object} clicked element to put active button class on
+ */
+function selectButton(clicked)
+{
+
+    unselectButtons('[data-menu]', 'navbar-link-active', exclusionClass);
+    //if the navbar item isn't a social media link (_blank)
+    if (!clicked.hasClass('social-icon'))
+    {
+        //add the highlighting class on it
+        clicked.addClass('navbar-link-active').trigger('changedActive');
+    }
+}
+
+/**
+ * Scroll the screen to element targetted
+ * @param {DOM element} element     the member of the navbar buttons array clicked
+ * @param {array}       targetsID   An array of the elements ID names to be targeted
+ */
+function scrollToElement(element, targetsID)
+{
+    //unselectButtons('.topbar-link', 'navbar-link-active', 'social-icon');
+    //
+    //gets 'element' index from the click handler
+    //targetsID must be an array of the scroll targets id property
+    //scrolls to the id vertical index
+    var index = element.data('menu');
+
+    if (!element.hasClass('social-icon'))
+    {
+        isScrolling = true;
+        $('html, body').animate(
+        {
+            scrollTop: $(targetsID[index]).offset().top + 1
+        }, 400, function()
+        {
+            isScrolling = false;
+            selectButton(element);
+        });
+    }
+}
+
+/**
+ * Select the matching button if the scroll index is passed on document
+ * @param  {array} targetsID  Array of string names of elements to markup
+ */
+function selectButtonOnScroll(targetsID)
+{
+    if (!isScrolling)
+    {
+        //iterate through the number of sections to define where the scroll
+        //position is
+        $.each(targetsID, function(index, value)
+        {
+            if (($(document).scrollTop() >= $(targetsID[index]).offset().top &&
+                    $(document).scrollTop() < $(targetsID[index + 1]).offset().top) && !$('[data-menu="' + index + '"]').hasClass('navbar-link-active'))
             {
-                $(this).removeClass(buttonActiveClass);
+                selectButton($('[data-menu="' + index + '"]'));
             }
         });
     }
+}
 
-    /**
-     * put class navbar-link-active on selected DOM element
-     * @param  {DOM object} clicked element to put active button class on
-     */
-    function selectButton(clicked)
-    {
-
-        unselectButtons('.topbar-link', 'navbar-link-active', 'social-icon');
-        //if the navbar item isn't a social media link (_blank)
-        if (!clicked.hasClass('social-icon'))
-        {
-            //add the highlighting class on it
-            clicked.addClass('navbar-link-active');
-        }
-    }
-
-    /**
-     * Scroll the screen to element targetted
-     * @param {DOM element} element     an object of the targeted elements
-     * @param {array}       targetsID   An array of the elements ID to be targeted
-     */
-    function scrollToElement(element, targetsID)
-    {
-        //unselectButtons('.topbar-link', 'navbar-link-active', 'social-icon');
-        //
-        //gets 'element' index from the click handler
-        //targetsID must be an array of the scroll targets id property
-        //scrolls to the id vertical index
-        var index = element.index();
-        if (!element.hasClass('social-icon'))
-        {
-            isScrolling = true;
-            $('html, body').animate(
-            {
-                scrollTop: $(targetsID[index]).offset().top + 1
-            }, 250, function()
-            {
-                isScrolling = false;
-                selectButton(element);
-            });
-        }
-    }
-
-    /**
-     * Select the matching button if the scroll index is passed on document
-     * @param  {array} targetsID  Array of string names of elements to markup
-     */
-    function selectButtonOnScroll(targetsID)
-    {
-        if (!isScrolling)
-        {
-            //iterate through the number of sections to define where the scroll
-            //position is
-            $.each(targetsID, function(index, value)
-            {
-                if ($(document).scrollTop() >= $(targetsID[index]).offset().top &&
-                    $(document).scrollTop() < $(targetsID[index + 1]).offset().top)
-                {
-                    selectButton($($('.topbar-link')[index]));
-                }
-            });
-        }
-    }
-
-});
+//});
