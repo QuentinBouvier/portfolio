d37e663f2d620f4b045a39452270c9422f48d12c
BabbleProd <32564163+QuentinBouvier@users.noreply.github.com>
2017-11-17 16:04:20 +0100

    v2 work, banner and navbar fixed

---
+++ html/v2/index.php
@@ -0,0 +1,14 @@
+<?php
+
+//template
+require_once("./templates/head.html");
+require_once("./templates/nav.html");
+
+// content
+require_once("./pages/banner.html");
+require_once("./pages/works.html");
+// require_once("./pages/photos.html");
+require_once("./pages/CV.html");
+
+// template
+require_once("./templates/foot.html");
