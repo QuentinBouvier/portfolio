a3015d4318e80f0f9143c7ab537b22f4ec4ff7ad
QuentinBouvier <32564163+QuentinBouvier@users.noreply.github.com>
2017-11-24 20:30:55 +0100

    now generates one tile

--- html/v2/pages/works/worksHandler.class.php
+++ html/v2/pages/works/worksHandler.class.php
@@ -6,18 +6,38 @@ class worksHandler
     private $_worksDir;
     private $_tileContDepth;
     private $_tileArch;
+    private $_figureClass;
+    private $_captionClass;
+    private $_jsonWorks;
+    
+    /**
+     *  tilesClass struct:
+     *      lenght = $_tileContDepth
+     *      [0] => flex-container flex-justify-center flex-wrap main-work
+     *      [1] => flex-container flex-columns project-tile fourth-width
+    */
     private $_tilesClass;    
 
-    public function __construct($worksDir, $tilesArch, $tilesClass)
+    public function __construct(string $worksDir)
     {
         $this->_tileContDepth = 2;
+        $this->_worksDir = $worksDir;
         $this->findWorks($worksDir);
+        var_dump($this->_works);
+        $this->_tilesClass = [
+            "flex-container flex-justify-center flex-wrap main-work", 
+            "flex-container flex-columns project-tile fourth-width"
+        ];
+        $this->_figureClass = "project-figure";
+        $this->_captionClass = "project-caption";
+        
+        $this->tileTemplate("bonjour", "../images/avatar.jpg", "Avatar dodécaèdre");
     }
 
     private function findWorks($dir)
     {
         foreach (array_slice(scandir($dir),2) as $cdir) {
-            $this->_work[] = $cdir;
+            $this->_works[] = $cdir;
         }
     }
 
@@ -28,17 +48,38 @@ class worksHandler
         }
     }
 
+    private function populateJson()
+    {
+
+    }
+
     private function tileTemplate($title, $figUrl, $figAlt)
     {
         ob_start(); ?>
             
+                <?php
+                    for ($i = 0; $i < $this->_tileContDepth; $i++)
+                    {
+                        echo "<div class='{$this->_tilesClass[$i]}'>";
+                    }
+                ?>
+                <div class=<?= "'$this->_figureClass'"; ?>>
+                    <img src="<?= $figUrl; ?>" alt="<?= $figAlt; ?>">
+                </div>
+                <span class=<?= "'$this->_captionClass'"; ?>>
+                    <?= $title; ?>
+                </span>
+                <?php
+                    for ($i = 0; $i < $this->_tileContDepth; $i++)
+                    {
+                        echo "</div>";
+                    }
+                ?>                
+            </div>
         <?php
         $this->_tileArch = ob_get_contents();
         ob_end_clean();
-    }
 
-    public function setTilesClass(array $TilesClass)
-    {
-
+        echo $this->_tileArch;
     }
 }
