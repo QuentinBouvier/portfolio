1ae36c83c2241d948521b62d1a426a819f3e2065
QuentinBouvier <32564163+QuentinBouvier@users.noreply.github.com>
2017-11-29 00:50:45 +0100

    changed classes, TODO, animate hide and displays

--- html/v2/js/works.js
+++ html/v2/js/works.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
 });
 
-var originalPos;
+var originalOrder = [];
 
 function worksInit()
 {
@@ -24,6 +24,7 @@ function worksInit()
 function dispAllTiles()
 {
     $('[data-work]').each(function(i, v){
+        originalOrder[i] = $(this).css('order');
         if ($(this).hasClass('active')) $(this).removeClass('active');
         if ($(this).hasClass('min')) $(this).removeClass('min');
     });
@@ -31,29 +32,36 @@ function dispAllTiles()
 
 function activate(target)
 {
-    $('[data-work]').each(function() 
+    var clicked;
+    $('[data-work]').each(function(i, v) 
     {
         if ($(this).data('work') != target)
-        {
+        {            
+            $(this).css('order', originalOrder[i]);
             $(this).removeClass('active');
             $(this).addClass('min');
         }
         else
-        {
+        {            
+            $(this).css('order', '-2');
             $(this).addClass('active');
             $(this).removeClass('min');
         }
     });
 
-    $('[data-work-preview]').each(function() {
+    $('[data-work-preview]').each(function(i, v) {
         
         if ($(this).data('work-preview') != target)
-        {
+        {            
+            $(this).css('order', originalOrder[i] + parseInt(5));
             $(this).addClass('hidden');
+            $(this).removeClass('active');
         }
         else
         {
+            $(this).css('order', '-1');
             $(this).removeClass('hidden');
+            $(this).addClass('active');
         }
     });
 }
