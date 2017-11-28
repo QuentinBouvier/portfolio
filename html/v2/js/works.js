2d8d3982b3f8dac52f9d97967ae582b4d0b2938c
QuentinBouvier <32564163+QuentinBouvier@users.noreply.github.com>
2017-11-28 07:49:03 +0100

    Selecting and changing pos of tiles

--- html/v2/js/works.js
+++ html/v2/js/works.js
@@ -6,16 +6,54 @@
 
 $(document).ready(function() {
 
-    dispAllTiles();
+    worksInit();
 
 });
 
-var idleClass = '';
-var miniLowerBarClass = '';
-var activeClass = '';
+var originalPos;
+
+function worksInit()
+{
+    dispAllTiles();
+    
+    $('[data-work]').on('click', function() {
+        activate($(this).data('work'));
+    });
+}
 
 function dispAllTiles()
 {
     $('[data-work]').each(function(i, v){
+        if ($(this).hasClass('active')) $(this).removeClass('active');
+        if ($(this).hasClass('min')) $(this).removeClass('min');
+    });
+}
+
+function activate(target)
+{
+    $('[data-work]').each(function() 
+    {
+        if ($(this).data('work') != target)
+        {
+            $(this).removeClass('active');
+            $(this).addClass('min');
+        }
+        else
+        {
+            $(this).addClass('active');
+            $(this).removeClass('min');
+        }
+    });
+
+    $('[data-work-preview]').each(function() {
+        
+        if ($(this).data('work-preview') != target)
+        {
+            $(this).addClass('hidden');
+        }
+        else
+        {
+            $(this).removeClass('hidden');
+        }
     });
 }
