e23a06394d0103d432c02069219350a52e8373dc
QuentinBouvier <32564163+QuentinBouvier@users.noreply.github.com>
2018-01-05 11:40:24 +0100

    init class

--- html/js/konami/classes/ThreeInstance.ts
+++ html/js/konami/classes/ThreeInstance.ts
@@ -1,1 +1,12 @@
+/// <reference path="../typings/index.d.ts"/>
+import "../inc/CanvasRenderer.js";
 
+export class ThreeInstance
+{
+    rendererWebGL: THREE.WebGLRenderer;
+    rendererCanvas: THREE.CanvasRenderer;
+    scene: THREE.Scene
+    camera: THREE.Camera
+    
+    static function 
+}
